c646b124a8c6a392aca2716ffb8ac892a699a321
Lahavas <uv187sn@naver.com>
2018-01-03 17:01:43 +0900

    create book title table view cell [create_DGBSearchViewController]

---
+++ Doksugongbang/Views/Table View Cells/DGBBookTitleTableViewCell.m
@@ -0,0 +1,19 @@
+//
+//  DGBBookTitleTableViewCell.m
+//  Doksugongbang
+//
+//  Created by USER on 2018. 1. 3..
+//  Copyright © 2018년 USER. All rights reserved.
+//
+
+#import "DGBBookTitleTableViewCell.h"
+
+@implementation DGBBookTitleTableViewCell
+
+#pragma mark - Class Methods
+
++ (NSString *)className {
+    return NSStringFromClass([self class]);
+}
+
+@end
