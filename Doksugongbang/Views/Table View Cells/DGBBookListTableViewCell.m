09a7b5125dca173cb5ca7fdc8d76937c0b62fbad
Lahavas <uv187sn@naver.com>
2018-01-04 12:18:51 +0900

    create book list table view cell [show_book_list]

---
+++ Doksugongbang/Views/Table View Cells/DGBBookListTableViewCell.m
@@ -0,0 +1,20 @@
+//
+//  DGBBookListTableViewCell.m
+//  Doksugongbang
+//
+//  Created by USER on 2018. 1. 4..
+//  Copyright © 2018년 USER. All rights reserved.
+//
+
+#import "DGBBookListTableViewCell.h"
+#import "DGBBookMainView.h"
+
+@implementation DGBBookListTableViewCell
+
+#pragma mark - Class Methods
+
++ (NSString *)className {
+    return NSStringFromClass([self class]);
+}
+
+@end
